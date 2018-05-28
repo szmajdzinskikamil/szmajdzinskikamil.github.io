cbd798b4f58794c4a775b5a13d4ee56303002fce
kamilszmajdzinski <kamil.szmajdzinski@gmail.com>
2018-05-28 13:28:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<!-- TODO -->
+
+<!-- ===
+1. CHANGE COLORS - done
+2. CHANGE LAYOUT -done
+3. LOADING SCREEN
+4. FOOTER -done
+5. CV - DONE
+6. MAYBE FONTS
+7. PORTFOLIO
+=== -->
+
+<!-- 
+==
+1. emoji
+2. spotify
+3. podwiez
+4. movies
+5. kshosting albo jakas stronka
+6. ztl kosxielec
+== -->
---
+++ index.js
@@ -0,0 +1,29 @@
+document.getElementById('one').addEventListener('click', function() {
+    var win = window.open('https://github.com/kamilszmajdzinski/emojillegro', '_blank');
+    win.focus();
+})
+
+document.getElementById('two').addEventListener('click', function() {
+    var win = window.open('https://github.com/kamilszmajdzinski/spotify-with-react', '_blank');
+    win.focus();
+})
+
+document.getElementById('three').addEventListener('click', function() {
+    var win = window.open('https://github.com/kamilszmajdzinski/podwiez-pl', '_blank');
+    win.focus();
+})
+
+document.getElementById('four').addEventListener('click', function() {
+    var win = window.open('https://github.com/kamilszmajdzinski/movieFinder', '_blank');
+    win.focus();
+})
+
+// document.getElementById('five').addEventListener('click', function() {
+//     var win = window.open('https://github.com/kamilszmajdzinski/emojillegro', '_blank');
+//     win.focus();
+// })
+
+// document.getElementById('one').addEventListener('click', function() {
+//     var win = window.open('https://github.com/kamilszmajdzinski/emojillegro', '_blank');
+//     win.focus();
+// })
